35f7aae95ae4a691fee1c1ffc6f5683aa05018aa
unknown <ph.hogphuc@gmail.com>
2020-12-19 20:19:34 +1300

    added deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t phhogphuc/multi-client:latest -t phhogphuc/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t phhogphuc/multi-server:latest -t phhogphuc/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t phhogphuc/multi-worker:latest -t phhogphuc/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push phhogphuc/multi-client:latest
+docker push phhogphuc/multi-server:latest
+docker push phhogphuc/multi-worker:latest
+
+docker push phhogphuc/multi-client:$SHA
+docker push phhogphuc/multi-server:$SHA
+docker push phhogphuc/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deyployments/client-deyployment client=phhogphuc/multi-client:$SHA
+kubectl set image deyployments/server-deyployment server=phhogphuc/multi-server:$SHA
+kubectl set image deyployments/worker-deyployment worker=phhogphuc/multi-worker:$SHA
